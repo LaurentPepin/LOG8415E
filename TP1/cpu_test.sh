4549b0e946ffb60229a5bce66a5ddeaf6e402e64
Tristan Mercille <tristanmercille@gmail.com>
2019-02-04 13:42:17 -0500

    cpu_test script working

--- TP1/cpu_test.sh
+++ TP1/cpu_test.sh
@@ -8,8 +8,6 @@ if [[ "$INSTANCE" == "" ]]; then
   exit 1
 fi
 
-#apt-get install sysbench --yes
-
 for power in {1..9}; do
   maxPrime=$((10**$power))
   totalTime=$(sysbench --test=cpu --cpu-max-prime=$maxPrime run | grep "total time:" | grep -oP "\d+\.\d+")
