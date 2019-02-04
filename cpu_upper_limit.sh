72f85cf11d548ef7664b2a4692e0753c1fd55097
Laurent Pepin <laurent.pepin@polymtl.ca>
2019-02-04 12:42:57 -0500

    Regex and interval and min max prime.

--- cpu_upper_limit.sh
+++ cpu_upper_limit.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-MAX_PRIME=$1
-N_THREADS=$2
 
-if [[ "$MAX_PRIME" == "" && "$N_THREADS" == "" ]]; then
-  echo "First parameter is max_prime"
-  echo "Second parameter is n_threads"
-else
-  sysbench --test=cpu --cpu-max-prime=$MAX_PRIME --num-threads=$N_THREADS run
-fi
+sudo apt-get install sysbench --yes
+
+MAX_PRIME = 10
+
+
+sysbench --test=cpu --cpu-max-prime=$MAX_PRIME run | grep "total time"
