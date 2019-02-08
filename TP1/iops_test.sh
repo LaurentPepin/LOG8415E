bcb273004a2f438dc60e90951041daf39f22a454
Laurent Pepin <laurent.pepin@polymtl.ca>
2019-02-08 11:11:26 -0500

    Add iops_test.

---
+++ TP1/iops_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INSTANCE=$1
+
+if [[ "$INSTANCE" == "" ]]; then
+  echo "First parameter is INSTANCE"
+  exit 1
+fi
+
+directory="/iopsTestFiles"
+
+for n1024Files in {1..5}
+  for iteration in {1..5}
+    bonnie++ -d $directory -s 0 -n $n1024Files:10000:1000 -m $INSTANCE -x 1 
+  done
+done
