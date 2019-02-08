7714d889686a7a675a6152f527f27cddbd2caf8a
Laurent Pepin <laurent.pepin@polymtl.ca>
2019-02-08 14:16:12 -0500

    Do 5 tests for network. Specify fixed server.

--- TP1/network_test.sh
+++ TP1/network_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 INSTANCE=$1
+SERVERID=$2
 RESULTS_FILE_NAME="network_test_results.csv"
 
-if [[ "$INSTANCE" == "" ]]; then
+if [[ "$INSTANCE" == "" || "$SERVERID" == "" ]]; then
   echo "First parameter is INSTANCE"
+  echo "Second parameter is SERVERID, find it with speedtest-cli --list"
   exit 1
 fi
 
@@ -13,7 +15,8 @@ if [ ! -f $RESULTS_FILE_NAME ]; then
 	echo Instance,$speedtestHeaders > $RESULTS_FILE_NAME
 fi
 
-for iteration in {1..1}; do
-	results=$(speedtest-cli --csv)
+# Results are in bit/s according to https://www.mankier.com/1/speedtest-cli#--bytes
+for iteration in {1..5}; do
+	results=$(speedtest-cli --csv --server $SERVERID)
 	echo $INSTANCE,$results >> $RESULTS_FILE_NAME
 done
