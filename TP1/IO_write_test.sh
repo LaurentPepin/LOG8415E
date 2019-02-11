d9b710169c90421c9638b0d1c72bef505e853f2b
LouisBoud <louis.boudreau4@gmail.com>
2019-02-11 12:21:32 -0500

    IO write test finished

--- TP1/IO_write_test.sh
+++ TP1/IO_write_test.sh
@@ -15,14 +15,24 @@ fi
 
 
 # oflag=dsync This option get rid of caching
+#minimum of 1024 bytes
 # max block size will be 2^31 -> 2GB
-for power in {1..31}; do
+for power in {10..31}; do
   blockSize=$((2**$power))
-  writeTime=0
+  meanTime=0
   for iteration in {1..5}; do
-    writeTime=$(python -c "print $(dd if=/dev/zero of=/tmp/test bs=$blockSize count=1 oflag=dsync 2>&1 | sed 1,2d | cut -d',' -f3 | grep -oP "\d+\.\d+") + $writeTime")
+    # writeTime=$(python -c "print $(dd if=/dev/zero of=/tmp/test bs=$blockSize count=1 oflag=dsync 2>&1 | sed 1,2d | cut -d',' -f4) + $writeTime")
+    result=$(dd if=/dev/zero of=/tmp/test bs=$blockSize count=1 oflag=dsync 2>&1 | sed 1,2d | cut -d',' -f4)
+    unit=$(echo $result | grep -oP "[a-zA-Z]+B/s$")
+    writeTime=$(echo $result | grep -oP "\d+\.*\d+")
+    if [[ "$unit" == "kB/s" ]]
+    then
+      writeTime=$(python -c "print ($writeTime / 1000.0)")
+    fi
+  meanTime=$(python -c "print $meanTime + $writeTime")
   done
-  writeTime=$(python -c "print $writeTime / 5.0")
+  meanTime=$(python -c "print $meanTime / 5.0")
+  echo $meanTime
   rm /tmp/test
   echo $INSTANCE,$blockSize,$writeTime,$readTime >> $WRITE_TEST_FILE_NAME
 done
