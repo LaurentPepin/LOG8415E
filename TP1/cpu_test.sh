6089b819e77dd4788076abf850cee06f49240315
Tristan Mercille <tristanmercille@gmail.com>
2019-02-11 16:12:57 -0500

    fix cpu test

--- TP1/cpu_test.sh
+++ TP1/cpu_test.sh
@@ -12,10 +12,11 @@ if [ ! -f $RESULTS_FILE_NAME ]; then
   echo "instance,max_prime,time" > $RESULTS_FILE_NAME
 fi
 
-for power in {1..9}; do
+for power in {1..8}; do
   maxPrime=$((10**$power))
+  totalTime=0
   for iteration in {1..5}; do
-    totalTime=$(python -c "print $(sysbench --test=cpu --cpu-max-prime=$maxPrime run | grep "total time:" | grep -oP "\d+\.\d+") + $totalTime")
+    totalTime=$(python -c "print $(sysbench --max-time=5 cpu --cpu-max-prime=$maxPrime run | grep "avg:" | grep -oP "\d+\.\d+") + $totalTime")
   done
   totalTime=$(python -c "print $totalTime / 5.0")
   echo $INSTANCE,$maxPrime,$totalTime >> $RESULTS_FILE_NAME
