4d5f3fad4aaa420d72e2df10e5b351b290053ac4
Laurent Pepin <laurent.pepin@polymtl.ca>
2019-02-13 12:04:07 -0500

    Linear regression tests.

---
+++ TP1/linearRegression/iops_regression.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+INSTANCE=$1
+N_1024_FILES=$2
+RESULTS_FILE_NAME="./results/iops_test_results.csv"
+
+if [[ "$INSTANCE" == "" || "$N_1024_FILES" == "" ]]; then
+  echo "First parameter is INSTANCE"
+  echo "Second parameter is N_1024_FILES"
+  exit 1
+fi
+
+if [ ! -f $RESULTS_FILE_NAME ]; then
+	echo Instance,numFiles,sequentialCreateBySec,sequentialCreateCPU%,\
+  sequentialReadBySec,sequentialReadCPU%,sequentialDeleteBySec,sequentialDeleteCPU%,\
+  randomCreateBySec,randomCreateCPU%,randomReadBySec,randomReadCPU%,randomDeleteBySec,randomDeleteCPU%,\
+  sequentialCreateLatency,sequentialReadLatency,sequentialDeleteLatency,randomCreateLatency,randomReadLatency,\
+  randomDeleteLatency > $RESULTS_FILE_NAME
+fi
+
+mkdir -p iopsTestFiles
+directory="./iopsTestFiles"
+
+results=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+rawResults=$(bonnie++ -d $directory -s 0 -n $N_1024_FILES:10000:1000 -m $INSTANCE -x 1)
+for index in {1..18}
+do
+  if [[ "$index" -gt 12 ]]; then
+    substring=$((30+$index))
+  else
+    substring=$((24+$index))
+  fi
+  subResult=$(echo $rawResults | cut -d ',' -f $substring)
+  if [[ "$subResult" == *"+"* ]]; then
+    subResult=0
+  fi
+  subResult=$(echo $subResult | cut -d 'u' -f 1)
+  results[$index-1]=$(python -c "print float($subResult)")
+done
+
+stringResults=${results[@]}
+echo $INSTANCE,$n1024Files,${stringResults// /,}
+echo $INSTANCE,$n1024Files,${stringResults// /,} >> $RESULTS_FILE_NAME
+
+rm -rf iopsTestFiles
