678e13c7ba714a1f92c08669d4f1c6ecd5622c71
Laurent Pepin <laurent.pepin@polymtl.ca>
2019-02-13 15:50:43 -0500

    main script modified to add disk test.

--- TP1/motherOfAll.sh
+++ TP1/motherOfAll.sh
@@ -13,30 +13,18 @@ yes | ./installation.sh
 
 for iteration in {1..5}; do
   
-  
+  # CPU
+
+  # IO
+
+  # IOPS
   ./iops_test.sh $INSTANCE
 
+  # Memory
+
+  # Disk
+  ./io_read_test.sh $INSTANCE $DISK_PARTITION
+
+  # Network
   ./network_test.sh $INSTANCE 911
 done
-
-./cpu_test.sh $INSTANCE
-
-./IO_latency_test.sh $INSTANCE
-
-./IO_read_test.sh $INSTANCE $DISK_PARTITION
-
-./IO_test.sh $INSTANCE
-
-BLOCK_SIZE_1=$(./IO_write_blocksize_test.sh $INSTANCE)
-
-./IO_write_load_test.sh $INSTANCE $BLOCK_SIZE_1
-
-./iops_test.sh $INSTANCE
-
-BLOCK_SIZE_2_READ=$(./memory_read_block_size_test.sh $INSTANCE)
-BLOCK_SIZE_2_WRITE=$(./memory_write_block_size_test.sh $INSTANCE)
-
-./memory_read_total_size_test.sh $INSTANCE $BLOCK_SIZE_2_READ
-./memory_write_total_size_test.sh $INSTANCE $BLOCK_SIZE_2_WRITE
-
-./network_test.sh $INSTANCE 911
