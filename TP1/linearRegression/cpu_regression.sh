d2a0185c552a735700a91f6f29d74def4701958c
LouisBoud <louis.boudreau4@gmail.com>
2019-02-14 10:43:50 -0500

    collect regression data for graphs

--- TP1/linearRegression/cpu_regression.sh
+++ TP1/linearRegression/cpu_regression.sh
@@ -2,9 +2,11 @@
 
 INSTANCE=$1
 TEST_FILE_NAME="./results/cpu_results.csv"
+REGRESSION_GRAPH_FILE_NAME="./results/cpu_regression_graph_results.csv"
 
 if [ ! -f $TEST_FILE_NAME ]; then
 	echo "instance, maxPrime" > $TEST_FILE_NAME
+    echo "prime, time" > $REGRESSION_GRAPH_FILE_NAME
 fi
 
 if [[ "$INSTANCE" == "" ]]; then
@@ -12,14 +14,14 @@ if [[ "$INSTANCE" == "" ]]; then
   exit 1
 fi
 
-maxTime=600000
+maxTime=1
 maxNumberOfEvents=1
-incrementRate=2
-foundMaxPrime=false
-maxPrime=1
+incrementRate=5
+foundPrimeToMaxTime=false
+maxPrime=10
 lastPrime=0
 
-while [ $foundMaxPrime != 1 ]; do
+while [ $foundPrimeToMaxTime != 1 ]; do
     numberOfEvents=$(sysbench --max-time=$maxTime cpu --cpu-max-prime=$maxPrime run | grep "total number of events:" | grep -oP "\d+$")
     echo "Actual prime:"
     echo $maxPrime
@@ -34,6 +36,35 @@ while [ $foundMaxPrime != 1 ]; do
         if [[ $incrementRate -gt 3 ]]; then
             incrementRate=$(($incrementRate-1))
         else 
+            foundPrimeToMaxTime=1
+        fi
+        maxPrime=$lastPrime
+    fi
+done
+
+echo "Found event limit:"
+echo $maxPrime
+
+incrementRate=5
+computationThreshold=3
+foundMaxPrime=false
+
+while [ $foundMaxPrime != 1 ]; do
+    computationTime=$(sysbench --max-time=$maxTime cpu --cpu-max-prime=$maxPrime run | grep "total time:" | grep -oP "\d+.\d+" | cut -d '.' -f1)
+    echo "Actual computation time:"
+    echo $computationTime
+    echo "Actual max prime:"
+    echo $maxPrime
+    echo $maxPrime, $computationTime >> $REGRESSION_GRAPH_FILE_NAME
+    if (( $computationTime < $computationThreshold )); then
+        lastPrime=$maxPrime
+        maxPrime=$(($maxPrime + 10**$incrementRate))
+    else
+        echo "Busted threshold with maxPrime:"
+        echo $maxPrime
+        if [[ $incrementRate -gt 4 ]]; then
+            incrementRate=$(($incrementRate-1))
+        else 
             foundMaxPrime=1
         fi
         maxPrime=$lastPrime
@@ -44,3 +75,8 @@ echo "Regression test over"
 echo "maxPrime is:"
 echo $maxPrime
 echo $INSTANCE,$maxPrime >> $TEST_FILE_NAME
+
+git add ./results/cpu_regression_graph_results.csv
+git add ./results/cpu_results.csv
+git commit -m "CPU regression results for $INSTANCE"
+git push 
--- TP1/linearRegression/io_write_regression.sh
+++ TP1/linearRegression/io_write_regression.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 INSTANCE=$1
-WRITE_TEST_FILE_NAME="./results/IO_write_results.csv"
+WRITE_TEST_FILE_NAME="./results/IO_results.csv"
+REGRESSION_GRAPH_FILE_NAME="./results/IO_regression_graph_results.csv"
 
 if [ ! -f $WRITE_TEST_FILE_NAME ]; then
 	echo "instance, maxCount" > $WRITE_TEST_FILE_NAME
+    echo "count, transferTime" > $REGRESSION_GRAPH_FILE_NAME
 fi
 
 if [[ "$INSTANCE" == "" ]]; then
@@ -21,6 +23,11 @@ lastCount=0
 while [ $foundMaxCount != 1 ]; do
     transferTime=$(dd if=/dev/zero of=/tmp/test bs=64K count=$count conv=fdatasync 2>&1 | sed 1,2d | cut -d ',' -f3 | grep -oP "\d+\.\d+" | cut -d '.' -f1)
     rm /tmp/test
+    echo "Actual transfer time:"
+    echo $transferTime
+    echo "Actual count:"
+    echo $count
+    echo $count, $transferTime >> $REGRESSION_GRAPH_FILE_NAME
     if (( $transferTime > $maxTime )); then
         echo "Busted Max Time with count:"
         echo $count
@@ -40,3 +47,8 @@ echo "Regression test over"
 echo "Count is:"
 echo $count
 echo $INSTANCE,$count >> $WRITE_TEST_FILE_NAME
+
+git add ./results/IO_regression_graph_results.csv
+git add ./results/IO_results.csv
+git commit -m "IO regression results for $INSTANCE"
+git push 
