c439befc65559cc75d869ed49bb950ce726a8bba
Ubuntu <ubuntu@ip-172-31-87-57.ec2.internal>
2019-02-15 20:47:58 +0000

    Memory regression test resutls for m4.large.

--- TP1/linearRegression/memory_regression.sh
+++ TP1/linearRegression/memory_regression.sh
@@ -17,10 +17,11 @@ fi
 # Run and increase stressors until we get the first memory error.
 memoryErrorsCount=0
 nStressors=1
-while [ $memoryErrorsCount == 0 ]; do
-    result=$(stress-ng --brk $nStressors --stack $nStressors --bigheap $nStressors --metrics-brief --timeout 60s)
-    echo result
-    memoryErrorsCount=$(echo results | grep -c "Cannot allocate memory")
+while [[ "$memoryErrorsCount" -eq 0 ]]; do
+	result="$(stress-ng --brk $nStressors --stack $nStressors --bigheap $nStressors --metrics-brief --timeout 60s 2>&1)"
+echo $result
+	memoryErrorsCount=$(echo $result | grep -o "Cannot allocate memory" * | wc -l)
+	echo "memoryErrors: "$memoryErrorsCount
     echo $INSTANCE,$nStressors,$memoryErrorsCount >> $RESULTS_FILE_NAME
 done
 echo "Regression test over"
