8cf5f18327e72476817b8268a164e7374a095200
LaurentPepin <laurent.pepin@polymtl.ca>
2019-02-18 15:05:40 -0500

    Adding echos.

--- TP1/motherOfAll.sh
+++ TP1/motherOfAll.sh
@@ -12,25 +12,31 @@ fi
 yes | ./installation.sh
 
 for iteration in {1..5}; do
-  
+  echo "\n************************ Starting CPU test"
   # CPU
   ./cpu_test.sh $INSTANCE
 
+  echo "************************ Starting IO test"
   # IO
   ./IO_write_test.sh $INSTANCE $DISK_PARTITION
   
+  echo "************************ Starting IO latency test"
   #IO latency
   ./IO_latency_test.sh $INSTANCE
   
+  echo "************************ Starting IOPS test"
   # IOPS
   ./iops_test.sh $INSTANCE
 
+  echo "************************ Starting Memory test"
   # Memory
   ./memory_test.sh $INSTANCE
 
+  echo "************************ Starting Disk test"
   # Disk
   ./io_read_test.sh $INSTANCE $DISK_PARTITION
 
+  echo "************************ Starting Network test"
   # Network
   ./network_test.sh $INSTANCE 911
 done
