626aa13fe28485dc6e77620b28df100fc1ff9c73
Tristan Mercille <tristanmercille@gmail.com>
2019-02-21 06:46:36 -0500

    mem graph script (all in one fig)

--- TP1/graphs/memory_graphs.py
+++ TP1/graphs/memory_graphs.py
@@ -8,25 +8,11 @@ import shutil
 
 instances = ['a1-large', 'c5-xlarge', 'c5-2xlarge', 'r5-large', 'h1-2xlarge']
 
-# brk
-for instance in instances:
-		df = pd.read_csv("./../results/memory_test_results_" + instance + ".csv")
-		plt.ylim(100000, 900000)
-		g = sns.relplot(x="iteration", y="brk", kind="line", data=df)
-		g.add_legend()
-		plt.savefig('memory_graph_' + instance + "_brk.png")
-# stack
-for instance in instances:
-		df = pd.read_csv("./../results/memory_test_results_" + instance + ".csv")
-		plt.ylim(0, 500)
-		g = sns.relplot(x="iteration", y="stack", kind="line", data=df)
-		g.add_legend()
-		plt.savefig('memory_graph_' + instance + "_stack.png")
+df = pd.DataFrame()
+df = pd.read_csv("./../results/memory_test_results_" + instances[0] + ".csv")
+for instance in instances[1:]:
+	df = pd.concat([df, pd.read_csv("./../results/memory_test_results_" + instance + ".csv")])
+df["index"] = pd.Series(df.index, index=df.index)
 
-# bigheap
-for instance in instances:
-		df = pd.read_csv("./../results/memory_test_results_" + instance + ".csv")
-		plt.ylim(9000, 58000)
-		g = sns.relplot(x="iteration", y="bigheap", kind="line", data=df)
-		g.add_legend()
-		plt.savefig('memory_graph_' + instance + "_bigheap.png")
+g = sns.lineplot(data=df, x="index", y="brk", hue="instance")
+plt.savefig("test.png")
