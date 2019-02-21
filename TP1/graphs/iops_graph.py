61e76dd7c795192a7778ebb2740a70d54e2ca9b9
LaurentPepin <laurent.pepin@polymtl.ca>
2019-02-21 18:04:07 -0500

    Graphs for iops.

--- TP1/graphs/iops_graph.py
+++ TP1/graphs/iops_graph.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env python3
+
 import sys
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-
 prefix = './../results/iops_test_results_'
 postfix = '.csv'
 instances = ['a1-large', 'c5-2xlarge', 'c5-xlarge', 'h1-2xlarge', 'r5-large']
 
-for instance in instances:
-    df = pd.read_csv(prefix+instance+postfix)
-    df = df.drop(columns=['Instance', 'numFiles', 'sequentialCreateCPU%', 'sequentialReadCPU%', 'sequentialDeleteCPU%', 'randomCreateCPU%', 'randomReadCPU%', 'randomDeleteCPU%',
-                          ' sequentialCreateLatency', 'sequentialReadLatency', 'sequentialDeleteLatency', 'randomCreateLatency', 'randomReadLatency', ' randomDeleteLatency'])
-    print(df)
-    df = df.melt('iteration', var_name='Test',  value_name='ops/sec')
-    g = sns.factorplot(x='iteration', y='ops/sec', hue='Test', data=df)
+# create dataset with all iops results from all instances
+df = pd.DataFrame()
+df = pd.read_csv(prefix + instances[0] + ".csv")
+for instance in instances[1:]:
+    df = pd.concat([df, pd.read_csv(prefix + instance + postfix)])
 
-    outputFile = './iops/graph_'+instance
-    plt.savefig(outputFile)
+df = df.drop(columns=['numFiles', 'sequentialCreateCPU%', 'sequentialReadCPU%', 'sequentialDeleteCPU%', 'randomCreateCPU%', 'randomReadCPU%', 'randomDeleteCPU%',
+                      ' sequentialCreateLatency', 'sequentialReadLatency', 'sequentialDeleteLatency', 'randomCreateLatency', 'randomReadLatency', ' randomDeleteLatency'])
+
+print(df)
+
+# Create 1 graph per measure (6 graphs)
+outputFolder = './iops/'
+measures = ['sequentialCreateBySec', ' sequentialReadBySec', 'sequentialDeleteBySec',
+            ' randomCreateBySec', 'randomReadBySec', 'randomDeleteBySec']
+for measure in measures:
+    g = sns.lineplot(data=df, x="iteration", y=measure,
+                     hue="Instance", marker='o')
+    plt.title("Variation of " + measure.lstrip() + " over 5 iterations")
+    plt.xticks([1, 2, 3, 4, 5])
+    plt.ylabel(measure + " (file/s)")
+    plt.xlabel("Iteration")
+    plt.subplots_adjust(right=0.8)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(outputFolder + measure.lstrip() + '.png')
+    plt.close()
